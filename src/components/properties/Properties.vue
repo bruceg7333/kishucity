45efe5d04a71b5232a97323466248074b2e7a56f
timcooker <timcooker@ggmail.com>
2025-10-17 17:17:36 +0800

    images

--- src/components/properties/Properties.vue
+++ src/components/properties/Properties.vue
@@ -28,12 +28,12 @@ export default {
   data() {
     return {
       properties: [
-        { id: 1, title: 'TOKYO', subtitle: 'By NIGO · Chiba, Japan', summary: '6 Beds + 1 Capsule Room, 3 Bathrooms', price: '6,387,000 USD' ,cover: '/public/properties/hotel1/cover.png'},
-        { id: 2, title: 'RUSUTSU', subtitle: 'By Snøhetta · Hokkaido, Japan', summary: '8 Beds, 4 Bathrooms', price: '7,884,000 USD' ,cover: '/public/properties/hotel1/cover.png'},
-        { id: 3, title: 'SETOUCHI (360/270/180)', subtitle: 'By Bjarke Ingels · Setouchi, Japan', summary: 'Varies by property', price: '2,662,000 USD' ,cover: '/public/properties/hotel1/cover.png'},
-        { id: 4, title: 'MASU', subtitle: 'By Masamichi Katayama · Kitakaruizawa, Japan', summary: '4-6 Beds, 1 Bathroom', price: '565,000 USD' ,cover: '/public/properties/hotel1/cover.png'},
-        { id: 5, title: 'CLUB SUITE', subtitle: 'Miura, Japan', summary: '2 Beds, 1 Bathroom', price: '466,000 USD' ,cover: '/public/properties/hotel1/cover.png'}, 
-        { id: 6, title: 'BASE L', subtitle: 'By Aizawa Yosuke · Kitakaruizawa, Japan', summary: '8 Beds, 1 Bathroom', price: '477,000 USD' ,cover: '/public/properties/hotel1/cover.png'},
+        { id: 1, title: 'TOKYO', subtitle: 'By NIGO · Chiba, Japan', summary: '6 Beds + 1 Capsule Room, 3 Bathrooms', price: '6,387,000 USD' ,cover: '/properties/hotel1/cover.png'},
+        { id: 2, title: 'RUSUTSU', subtitle: 'By Snøhetta · Hokkaido, Japan', summary: '8 Beds, 4 Bathrooms', price: '7,884,000 USD' ,cover: '/properties/hotel1/cover.png'},
+        { id: 3, title: 'SETOUCHI (360/270/180)', subtitle: 'By Bjarke Ingels · Setouchi, Japan', summary: 'Varies by property', price: '2,662,000 USD' ,cover: '/properties/hotel1/cover.png'},
+        { id: 4, title: 'MASU', subtitle: 'By Masamichi Katayama · Kitakaruizawa, Japan', summary: '4-6 Beds, 1 Bathroom', price: '565,000 USD' ,cover: '/properties/hotel1/cover.png'},
+        { id: 5, title: 'CLUB SUITE', subtitle: 'Miura, Japan', summary: '2 Beds, 1 Bathroom', price: '466,000 USD' ,cover: '/properties/hotel1/cover.png'}, 
+        { id: 6, title: 'BASE L', subtitle: 'By Aizawa Yosuke · Kitakaruizawa, Japan', summary: '8 Beds, 1 Bathroom', price: '477,000 USD' ,cover: '/properties/hotel1/cover.png'},
       ],
     };
   },
